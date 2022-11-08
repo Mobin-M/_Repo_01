75afd2c433bd6536ee55bddd2c87b49935f9096e
Mobin <mobinmohammadnia@gmail.com>
2022-11-08 20:21:33 +0100

    --

--- 03_SoundDesign/temp.py
+++ 03_SoundDesign/temp.py
@@ -1,7 +1,3 @@
-# try to make a console app. getting a number from the user and start to count down and print the number at each step.
-# while doing the count down and printing, the program should be always ready to take a new input and restart the count down.
-
-
-# How to get user input in python?
-# How to get user input and update the sound/printing being played, WHILE playing/printing the previous sound/text.
-# --> concurrent? thread? multiprocess? , ....?
+# How to get user input in python three at the same time with default values?
+name = input("Enter your name: ")
+print(f"Hello, {int(name)-1}")
