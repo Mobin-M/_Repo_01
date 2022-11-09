72147fee8547c946d43ef5e6423e128dffa61e4d
Mobin <mobinmohammadnia@gmail.com>
2022-11-09 14:39:51 +0100

    user input added

--- 03_SoundDesign/temp.py
+++ 03_SoundDesign/temp.py
@@ -1,1 +1,16 @@
-# How to get user input in python three at the same time with default values?
+# Global variable
+f_comment = 0       # global flag to show flow comments
+
+# Get sinusoid parameter from the user with default values
+def get_sin_param():
+    if f_comment: print("inside get_sin_param")
+    freq = float(input("Please enter the frequency: ") or '0')
+    amp = float(input("Please enter the amplitude: ") or '0')
+    phi = float(input("Please enter the phase: ") or '0')
+    return freq, amp, phi
+
+
+
+
+freq, amp, phi = get_sin_param()
+print(freq, amp, phi)
